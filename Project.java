cb533b7e5020c8cc01b6e3820c1c406a5b393255
sahmed <s.ahmed.the1st@gmail.com>
2026-01-25 22:49:26 +0100

    Project work begin

--- Project.java
+++ Project.java
@@ -1,7 +1,18 @@
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Project {
     private int id;
     private String name;
     private Portfolio portfolio;
+
+    private LocalDate starDate;
+    private LocalDate targetEndDate;
+    private LocalDate actualEndDate;
+
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
 
     // Constructors
     public Project(String name, Portfolio portfolio) {
@@ -13,13 +24,34 @@ public class Project {
     // Setters
     public void setName(String name) {
         this.name = name;
+        this.updated_at = LocalDateTime.now();
     }
 
     public void setPortfolio(Portfolio portfolio) {
         this.portfolio = portfolio;
+        this.updated_at = LocalDateTime.now();
     }
 
+    public void setStarDate(LocalDate starDate) {
+        this.starDate = starDate;
+        this.updated_at = LocalDateTime.now();
+    }
+
+    public void setTartgetEndDate(LocalDate targetEndDate) {
+        this.targetEndDate = targetEndDate;
+        this.updated_at = LocalDateTime.now();
+    }
+
+    public void setActualEndDate(LocalDate actualEndDate) {
+        this.actualEndDate = actualEndDate;
+        this.updated_at = LocalDateTime.now();
+    }       
+    
     // Getters
+    public int getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
@@ -27,5 +59,25 @@ public class Project {
     public Portfolio getPortfolio() {
         return portfolio;
     }
+
+    public LocalDate getStarDate() {
+        return starDate;
+    }
+    
+    public LocalDate getTargetEndDate() {
+        return targetEndDate;
+    }
+
+    public LocalDate getActualEndDate() {
+        return actualEndDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return created_at;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updated_at;
+    }   
     
 }
